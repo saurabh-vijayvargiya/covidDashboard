c8ec3253f077c1db844bb98af687b0881058594d
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2020-04-18 16:32:25 +0530

    Configured the app and drawing chart on homepage

--- firstUI/views.py
+++ firstUI/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import pandas as pd
 
 # Create your views here.
+def index(request):
+    confirmedGlobal = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+    totalCount = confirmedGlobal[confirmedGlobal.columns[-1]].sum()
+
+    varA = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+    context = {
+        'totalCount': totalCount,
+        'varA': varA
+    }
+    return render(request, 'index.html', context)
